fe6c1d7a05985397fa1ddfdb19d9a86b8adf489f
Yevhenii Parasochka <singularis314@gmail.com>
2022-04-21 21:56:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+There is amateur full-stack project 
+
+Front - React
+
+Back - Go
+
+Infrastructure k8s
+
+Bellow all steps that need to be done in order to bring it to life. 
+
+Bash
+1) Init scrip: init-setup.sh
+
+Kubernetes
+1) Setup NGINX:
+    k apply -f nginx-service+deployment.yaml 
+    #Install the NGINX Ingress Controller
+    helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+    helm repo update
+    helm install ingress-nginx ingress-nginx/ingress-nginx
+
+
---
+++ init-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Install soft
+config_file=full-stack-kubeconfig.yaml 
+sudo apt install git wget vim bash-completion curl elinks
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+source /usr/share/bash-completion/bash_completion
+#k8s completion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
+#Import Linode ssh
+cd ../linode || exit
+chmod go-r "$config_file"
+export KUBECONFIG=$config_file
+#Install HELM
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
